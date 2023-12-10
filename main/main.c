833a1a3052741e3a4e44ef148834ef9579654d7b
TranMinhDuc <minhductran1432001@gmail.com>
2023-12-10 12:16:06 +0700

    first commit

---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "dht11.h"
+#include "driver/gpio.h"
+
+
+void app_main(void)
+{
+    DHT11_init(GPIO_NUM_4);
+
+    while(1) {
+        printf("Temperature is %d \n", DHT11_read().temperature);
+        printf("Humidity is %d\n", DHT11_read().humidity);
+        printf("Status code is %d\n", DHT11_read().status);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+
